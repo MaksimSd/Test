f11d61ef547e1cee43ce00e9bca8f44c4c56839f
Maksim <example@gmail.com>
2022-05-30 22:56:38 +0300

    change the greeting

---
+++ js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+const answ =[];
+
+answ[0] = prompt ('Как Вас зовут?', '');
+answ[1] = prompt ('Как Ваша фамилия?', '');
+answ[2] = prompt ('Сколько Вам лет?', '');
+
+document.write(answ);
